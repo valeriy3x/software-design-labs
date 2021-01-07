d865379a797a5fb5c87409aff44b9004ff1df4b5
valeriy3x <valeriykurbyko@gmail.com>
2021-01-07 17:24:24 +0300

    Add credentials validation

--- FirebaseGame/app/src/main/java/by/bsuir/firebasegame/viewmodels/AuthViewModel.kt
+++ FirebaseGame/app/src/main/java/by/bsuir/firebasegame/viewmodels/AuthViewModel.kt
@@ -2,12 +2,35 @@ package by.bsuir.firebasegame.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.navigation.compose.navArgument
 import by.bsuir.firebasegame.utilities.AuthNavigation
 import by.bsuir.firebasegame.utilities.SingleLiveEvent
 
 class AuthViewModel: ViewModel() {
     var navigation = SingleLiveEvent<AuthNavigation>()
     var progress = MutableLiveData<Boolean>(false)
+    val errorMessageEmail: SingleLiveEvent<String?> = SingleLiveEvent()
+    val errorMessagePassword: SingleLiveEvent<String?> = SingleLiveEvent()
+    
+    fun register(email: String, password: String) {
+        if(email.isNotEmpty() && password.isNotEmpty()) {
+            //register
+        }
+        validateCredentials(email, password)
+    }
+
+    fun login(email: String, password: String) {
+        if(email.isNotEmpty() && password.isNotEmpty()) {
+            //Login
+        }
+        validateCredentials(email, password)
+    }
+
+    private fun validateCredentials(email: String, password: String) {
+        errorMessageEmail.value = if (email.isEmpty()) emptyLinesErrorMessage else null
+        errorMessagePassword.value = if (password.isEmpty()) emptyLinesErrorMessage else null
+        // TODO:Fucking single live event doesn't work in this case, fix later
+    }
 
     fun redirectLoginToRegister() {
         navigation.value = AuthNavigation.LoginToRegister
@@ -15,5 +38,9 @@ class AuthViewModel: ViewModel() {
 
     fun redirectRegisterToLogin() {
         navigation.value = AuthNavigation.RegisterToLogin
+    }
+
+    companion object {
+        private const val emptyLinesErrorMessage = "Blank line is deprecated!"
     }
 }
---
+++ FirebaseGame/app/src/main/java/by/bsuir/firebasegame/viewmodels/BindingAdapters.java
@@ -0,0 +1,12 @@
+package by.bsuir.firebasegame.viewmodels;
+
+import androidx.databinding.BindingAdapter;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class BindingAdapters {
+    @BindingAdapter("app:errorText")
+    public static void setErrorMessage(TextInputLayout view, String errorMessage) {
+        view.setError(errorMessage);
+    }
+}
